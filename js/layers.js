0ceaae3861bb503011970a710508017e1c6710ee
pickletato <63068001+pickletato@users.noreply.github.com>
2021-06-24 18:36:25 -0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -22,7 +22,7 @@ addLayer("w", {
     },
     row: 0, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
-        {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+        {key: "w", description: "W: hire workers", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
     layerShown(){return true}
 })
